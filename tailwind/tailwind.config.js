62e87a920b6a9dd6d1b610996ee3bfea322c32f9
Finsa Nurpandi <finsaworks@gmail.com>
2023-09-28 22:32:39 +0700

    tailwindcss

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
