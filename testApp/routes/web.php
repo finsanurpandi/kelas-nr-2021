a9f946d32028f93d365b8b5ed03168aacb6e34fc
Finsa Nurpandi <finsaworks@gmail.com>
2023-10-06 09:32:50 +0700

    pembahasan laravel basic

---
+++ testApp/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/test', function () {
+    return view('sample.welcome');
+});
+
+Route::view('/welcome', 'auth.login');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/coba/{name?}', function($name = null) {
+    if ($name == null) {
+        return "Hello there...";
+    } else {
+        return "Hello, ".$name;
+    }
+});
+
+// <form method="post" action="{{ route('profile', ['finsa', 'kelas-nr', '2021']) }}">
